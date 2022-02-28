76e64f5cd958ab3bad01bcf2ec5ab8d4140f8a9b
Zylovue <dorry457@nate.com>
2022-02-28 23:43:19 +0900

    infinite timeout

--- rpicam.sh
+++ rpicam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-raspivid -w 1080 -h 720 -fps 30 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.0.5 port=6001
+raspivid -w 1080 -h 720 -fps 30 -b 2000000 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.0.5 port=6001
